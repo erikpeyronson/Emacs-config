fbd2791c93b51fc8b0a239dec01467cc9fc2a816
Erik Peyronson <erikpeyronson@gmail.com>
2018-08-26 18:37:38 +0200

    Another restart, this time org based

--- init.el
+++ init.el
@@ -1,80 +1,25 @@
 ;; init.el --- Emacs init-file
 
-;; Author: Erik Peyronson
-
-;;; Commentary:
-;; Emacs configuration used for c++, arduio, elisp, python and erlang
-;; development.
-;;
-;; Tested with Emacs 25.1.1
-;;
-;; Features:
-;; - Automagically installs and configures useful plugins and modes
-;;   using the use-package macro (https://github.com/jwiegley/use-package)
-;; - Sets up google-c-style indentation and formatting.
-;; - Moves auto-save files to ~/.emacs.d/tmp to avoid cluttering source
-;;   folders
-;; - Provides utility functions
-
-;;; Code:
-(setq gc-cons-threshold 1000000000)
-
-;; (package-initialize)
-(let ((default-directory  "~/.emacs.d/lisp/"))
-  (normal-top-level-add-subdirs-to-load-path))
-
-;; Create machine local files on first start
-(unless (file-exists-p (concat user-emacs-directory "lisp/local/cfg-custom-variables.el"))
-	(copy-file (concat user-emacs-directory "resources/cfg-custom-variables.el")
-		   (concat user-emacs-directory "lisp/local/cfg-custom-variables.el")))
-(unless (file-exists-p (concat user-emacs-directory "lisp/local/init-local.el"))
-  (write-region "" nil (concat user-emacs-directory "lisp/local/init-local.el")))
-
-;;; Configuration flags use to toggle functionality
-(require 'cfg-custom-variables)
-
-;;; General configuration, provides dependencies to alot of the
-
-(require 'gen-utils)
-(require 'gen-appearance)
-(require 'gen-behaviour)
-(require 'gen-key-bindings)
-(require 'gen-org)
-
-(when (>= emacs-major-version 25)
-  (require 'gen-package)
-  ;; Language initialization
-  (when cfg-cpp-enable (require 'lang-c++))
-  (when cfg-python-enable (require 'lang-python))
-  (when cfg-erlang-enable (require 'lang-erlang))
-  (when cfg-emacs-lisp-enable (require 'lang-emacs-lisp))
-  (when cfg-go-enable (require 'lang-go))
-  (when cfg-markdown-enable(require 'lang-markdown))
-  (when cfg-yaml-enable(require 'lang-yaml))
-  (when cfg-protobuf-enable(require 'lang-protobuf))
-
-  ;; Plugin configurations
-  (require 'plugin-fill-column-indicator)
-  (require 'plugin-highlight-indent-guides)
-  (require 'plugin-idle-highlight-mode)
-  (require 'plugin-desktop+)
-  (require 'plugin-magit)
-  (require 'plugin-yasnippet)
-  (require 'plugin-ido-mode)
-  (require 'plugin-company)
-  (require 'plugin-flycheck)
-  (require 'plugin-projectile)
-  (require 'plugin-ggtags)
-  (require 'plugin-god-mode)
-
-  ;; Standalone
-  (require 'plugin-eval-replace))
-
-;; Load local configuration found in lisp/local/init-local
-(load-file (concat user-emacs-directory "lisp/local/init-local.el"))
-;; Custom variables in separate files
-(setq custom-file "~/.emacs.d/emacs-custom.el")
-(unless (file-exists-p custom-file) (write-region "" nil custom-file))
-(load custom-file)
-;;; init.el ends here
-(put 'upcase-region 'disabled nil)
+;; Code:
+(package-initialize)
+(require 'org)
+(org-babel-load-file
+ (expand-file-name "config.org"
+                   user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(package-selected-packages
+   (quote
+    (solarized-theme bind-map use-package projectile ivy flycheck evil-surround evil-rsi evil-nerd-commenter evil-iedit-state evil-exchange evil-escape company))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
