422f725f3cee0efbd5da4e021a20305916d1b953
Erik Peyronson <erikpeyronson@gmail.com>
2018-08-27 18:31:36 +0200

    Keep backup files out of source tree and custom vars in separate file

--- init.el
+++ init.el
@@ -6,21 +6,3 @@
 (org-babel-load-file
  (expand-file-name "config.org"
                    user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
- '(org-src-fontify-natively t)
- '(package-selected-packages
-   (quote
-    (expand-region org-evil solarized-theme bind-map use-package projectile ivy flycheck evil-surround evil-rsi evil-nerd-commenter evil-iedit-state evil-exchange evil-escape company))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
